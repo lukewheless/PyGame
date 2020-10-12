bca20b2778070cb90b8c1f0629fabc085fd94fc3
Wheless <Luke_Wheless@baylor.edu>
2020-10-12 17:42:42 -0500

    enter

--- alien.py
+++ alien.py
@@ -1,11 +1,11 @@
 import pygame
-from pygame.sprite import sprite
+from pygame.sprite import Sprite
 
 class Alien(Sprite):
 
     def __init__(self, ai_game):
         super().__init__()
-        self.screen = ai.game.screen
+        self.screen = ai_game.screen
         self.settings = ai_game.settings
 
         self.image = pygame.image.load("images/alien.bmp")
@@ -14,7 +14,7 @@ class Alien(Sprite):
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        self.x =float(self.rect.x)
+        self.x = float(self.rect.x)
     
     def check_edges(self):
         screen_rect = self.screen.get_rect()
